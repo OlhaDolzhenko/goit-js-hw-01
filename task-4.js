4f6f6c02dfb88e8dc0250171ac6a2e2bf77d409b
Olha Dolzhenko <olyasorokolat@gmail.com>
2020-12-01 02:04:50 +0100

    Commit 1

---
+++ task-4.js
@@ -0,0 +1,19 @@
+const credits = 23580;
+const pricePerDroid = 3000;
+let message;
+let amount = prompt("Введите количество дроидов");
+
+if (amount === null) {
+  message = "Отменено пользователем!";
+} else {
+  amount = Number(amount);
+  let totalPrice = pricePerDroid * amount;
+  if (totalPrice > credits) {
+    message = "Недостаточно средств на счету!";
+  } else {
+    let balance = credits - totalPrice;
+    message = `Вы купили ${amount} дроидов, на счету осталось ${balance} кредитов.`;
+  }
+}
+
+console.log(message);
